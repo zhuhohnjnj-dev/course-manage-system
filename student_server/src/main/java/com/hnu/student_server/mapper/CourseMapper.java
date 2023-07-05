43c62d0fdc055b7c978724362708dc587d10ba73
zhuhohnjnj-dev <994854001@qq.com>
2023-07-05 20:45:08 +0800

    feature course

--- student_server/src/main/java/com/hnu/student_server/mapper/CourseMapper.java
+++ student_server/src/main/java/com/hnu/student_server/mapper/CourseMapper.java
@@ -1,6 +1,27 @@
 package com.hnu.student_server.mapper;
 
+import com.hnu.student_server.entity.Course;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+@Mapper
 public interface CourseMapper {
+    //    select
+    public List<Course> findBySearch(@Param("cid") Integer cid,
+                                     @Param("cname") String cname, @Param("fuzzy") Integer fuzzy,
+                                     @Param("lowBound") Integer lowBound, @Param("highBound") Integer highBound);
 
+    //    insert
+    public boolean insertCourse(@Param("course") Course course);
+
+    //    update
+    public boolean updateById(@Param("course") Course course);
+
+    //    delete
+    public boolean deleteById(@Param("cid") Integer cid);
 }
+
